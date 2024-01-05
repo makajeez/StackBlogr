87422af0f804c85b16467c68aedd163fed244fd2
Makajeez <buhari.alhassan0@gmail.com>
2024-01-05 18:51:52 +0100

    Configured Flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
+  imports: [CommonModule, HttpClientModule, RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'StackBlogr';
+
+  ngOnInit() {
+    initFlowbite()
+  }
 }
