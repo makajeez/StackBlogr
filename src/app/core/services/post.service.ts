a06755b61236e3cb8277360fcacdfe9affef9c11
Makajeez <buhari.alhassan0@gmail.com>
2024-01-05 18:55:25 +0100

    Service: Post service methods

--- src/app/core/services/post.service.ts
+++ src/app/core/services/post.service.ts
@@ -1,51 +1,55 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpErrorResponse } from '@angular/common/http';
+import { HttpClientModule, HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
-import { PostData } from '../models/post-data';
+import { PostData, ReturnedData } from '../models/post-data';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostService {
-  private baseUrl = "https://dummyapi.io/data/v1/";
+  private baseUrl = environment.baseUrl;
+  headers: HttpHeaders = new HttpHeaders();
+  
+  constructor(private _http: HttpClient) { 
+    this.headers = this.headers.append('app-id', environment.apiId);
+  }
 
-  constructor(private http: HttpClient) { }
-
-  getPost(id: number, page: number = 1, limit: number = 10): Observable<PostData> {
-    return this.http.get(`${this.baseUrl}post/${id}?page=${page}&limit=${limit}`)
+  getPost(id: string, page: number = 1, limit: number = 10): Observable<ReturnedData> {
+    return this._http.get(`${this.baseUrl}post/${id}?page=${page}&limit=${limit}`, {headers: this.headers})
     .pipe(map((response: any) => response),
     catchError((error: HttpErrorResponse) => {
       return throwError(error);
     }));
   }
   
-  getPosts(page: number = 1, limit: number = 10): Observable<PostData> {
-    return this.http.get(`${this.baseUrl}post?page=${page}&limit=${limit}`)
+  getPosts(page: number = 1, limit: number = 10): Observable<ReturnedData> {
+    return this._http.get(`${this.baseUrl}post?page=${page}&limit=${limit}`, {headers: this.headers})
     .pipe(map((response: any) => response),
     catchError((error: HttpErrorResponse) => {
       return throwError(error);
     }));
   }
 
-  getPostsByUser(id: number, page: number = 1, limit: number = 10): Observable<PostData> {
-    return this.http.get(`${this.baseUrl}user/${id}/post?page=${page}&limit=${limit}`)
+  getPostsByUser(id: string, page: number = 1, limit: number = 10): Observable<ReturnedData> {
+    return this._http.get(`${this.baseUrl}user/${id}/post?page=${page}&limit=${limit}`, {headers: this.headers})
     .pipe(map((response: any) => response),
     catchError((error: HttpErrorResponse) => {
       return throwError(error);
     }));
   }
 
-  getPostsByTag(id: number, page: number = 1, limit: number = 10): Observable<PostData> {
-    return this.http.get(`${this.baseUrl}tag/${id}/post?page=${page}&limit=${limit}`)
+  getPostsByTag(id: string, page: number = 1, limit: number = 10): Observable<ReturnedData> {
+    return this._http.get(`${this.baseUrl}tag/${id}/post?page=${page}&limit=${limit}`, {headers: this.headers})
     .pipe(map((response: any) => response),
     catchError((error: HttpErrorResponse) => {
       return throwError(error);
     }));
   }
 
-  createPost(post: PostData): Observable<PostData> {
-    return this.http.post(`${this.baseUrl}post/create`, post).pipe(
+  createPost(post: PostData): Observable<ReturnedData> {
+    return this._http.post(`${this.baseUrl}post/create`, post, {headers: this.headers}).pipe(
       map((response: any) => response),
       catchError((error: HttpErrorResponse) => {
         return throwError(error);
@@ -53,16 +57,16 @@ export class PostService {
     )
   }
 
-  updatePost(id: number, post: PostData): Observable<PostData> {
-    return this.http.put(`${this.baseUrl}post/${id}`, post)
+  updatePost(id: string, post: PostData): Observable<ReturnedData> {
+    return this._http.put(`${this.baseUrl}post/${id}`, post, {headers: this.headers})
     .pipe(map((response: any) => response),
     catchError((error: HttpErrorResponse) => {
       return throwError(error);
     }));
   }
 
-  deleteUser(id: number): Observable<PostData> {
-    return this.http.delete(`${this.baseUrl}post/${id}`).pipe(
+  deletePost(id: string): Observable<ReturnedData> {
+    return this._http.delete(`${this.baseUrl}post/${id}`, {headers: this.headers}).pipe(
       map((response: any) => response),
       catchError((error: HttpErrorResponse) => {
         return throwError(error);
