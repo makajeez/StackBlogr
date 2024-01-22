84200ff26932e79992842db6c714bbd12c8c8028
Makajeez <buhari.alhassan0@gmail.com>
2024-01-22 18:59:20 +0100

    Refactor: Nav Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { PostComponent } from './components/posts/post.component';
+import { NavComponent } from './shared/components/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, RouterOutlet, PostComponent],
+  imports: [
+    CommonModule, 
+    HttpClientModule, 
+    RouterOutlet, 
+    PostComponent,
+    NavComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
