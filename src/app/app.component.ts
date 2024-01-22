7cd8c3327bfd0dc490c8432718d2713e255559e2
Makajeez <buhari.alhassan0@gmail.com>
2024-01-22 18:12:54 +0100

    Path Change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './modules/home/home.component';
 import { initFlowbite } from 'flowbite';
+import { PostComponent } from './components/posts/post.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, RouterOutlet, HomeComponent],
+  imports: [CommonModule, HttpClientModule, RouterOutlet, PostComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
