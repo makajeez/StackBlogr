55d93d64a52ac3d291ed5251227ec7c055b11207
Makajeez <buhari.alhassan0@gmail.com>
2024-01-22 18:59:48 +0100

    Configured Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PostDetailsComponent } from './components/post-details/post-details.component';
+import { PostComponent } from './components/posts/post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'posts', pathMatch: 'full'},
+    { path: 'posts',  component: PostComponent },
+    { path: 'post/:id', component: PostDetailsComponent },
+];
