e32f6f39fce0153b3cc10ecb11963c61f17f3e37
Makajeez <buhari.alhassan0@gmail.com>
2024-02-03 20:57:53 +0100

    added ngrx selectors

--- src/app/store/post/post.selectors.ts
+++ src/app/store/post/post.selectors.ts
@@ -12,6 +12,16 @@ export const total = createSelector(
   (state: IPostState) => state.Posts.total
 );
 
+export const page = createSelector(
+  selectPostState,
+  (state: IPostState) => state.Posts.page
+);
+
+export const limit = createSelector(
+  selectPostState,
+  (state: IPostState) => state.Posts.limit
+);
+
 export const selectLoading = createSelector(
   selectPostState,
   (state: IPostState) => state.loading
