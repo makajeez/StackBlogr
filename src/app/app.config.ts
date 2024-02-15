657653e316fc49c1498eaeb9e8ae4a9df68b1f32
Makajeez <buhari.alhassan0@gmail.com>
2024-02-15 19:28:00 +0100

    configured router store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,23 +8,24 @@ import { postReducer } from './store/post/post.reducer';
 import { PostEffects } from './store/post/post.effects';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreDevtoolsModule, provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideRouterStore } from '@ngrx/router-store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     // provideRouter(routes),
+    provideRouterStore(),
     importProvidersFrom(
-      RouterModule.forRoot(routes), 
-      HttpClientModule, 
-      StoreModule.forRoot({
-        Posts: postReducer
-      }), 
-      StoreDevtoolsModule.instrument({ 
-        maxAge: 25, 
-        logOnly: !isDevMode(),
-        autoPause: true 
-      }), 
-      EffectsModule.forRoot([PostEffects])
-      ),
-      
+        RouterModule.forRoot(routes),
+        HttpClientModule, 
+        EffectsModule.forRoot([PostEffects]),
+        StoreModule.forRoot({
+            Posts: postReducer
+        }), 
+        StoreDevtoolsModule.instrument({
+            maxAge: 25,
+            logOnly: !isDevMode(),
+            autoPause: true
+        }), 
+        ),
 ]
 };
