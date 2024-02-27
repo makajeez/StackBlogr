c298ab7105e5682606f53ee99b6085bd97fca4c1
Makajeez <buhari.alhassan0@gmail.com>
2024-02-27 12:55:43 +0100

    post details component

--- src/app/components/post-details/post-details.component.ts
+++ src/app/components/post-details/post-details.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { PostService } from '../../core/services/post.service';
+import { PostData } from '../../core/models/post-data';
 
 @Component({
   selector: 'app-post-details',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './post-details.component.html',
   styleUrl: './post-details.component.css'
 })
-export class PostDetailsComponent {
+export class PostDetailsComponent implements OnInit{
 
+  data$!: PostData;
+
+
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private postService: PostService
+    ) {
+
+  }
+  
+  ngOnInit() {
+    // let id = this.activateRoute.snapshot.paramMap.get('id');
+    
+    //  this.postService.getPost(`${id}`).subscribe({
+    //   next: data => {
+    //     this.data$ = data;
+    //   }
+    // })
+    this.activateRoute.data.subscribe({
+      next: (data: any) => {
+        this.data$ = data
+      }
+    })
+  }
 }
