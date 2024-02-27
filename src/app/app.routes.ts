d608eca87de78e0770e3e0aace1f669e67f78dfe
Makajeez <buhari.alhassan0@gmail.com>
2024-02-27 12:55:12 +0100

    route resolver

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { PostDetailsComponent } from './components/post-details/post-details.component';
 import { PostComponent } from './components/posts/post.component';
+import { postDetailsResolver } from './components/post-details/post-details.resolver';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'posts', pathMatch: 'full'},
     { path: 'posts',  component: PostComponent },
-    { path: 'post/:id', component: PostDetailsComponent },
+    { 
+        path: 'post/:id', 
+        component: PostDetailsComponent,
+        resolve: {postDetails: postDetailsResolver}
+     },
 ];
